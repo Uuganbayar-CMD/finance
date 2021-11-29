0119b3f9bf02a752c7b74c0e931b9aa0af6c1a78
uuganbayar <d.uuganbayar.stu@gmail.com>
2021-11-29 22:26:13 +0800

    item add hiideg bolgono

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ var financeController =(function(){
         };
 
        var data = {
-           allItems: {
+           items: {
                inc:[],
                exp:[]
            },
@@ -48,7 +48,30 @@ var financeController =(function(){
                inc:0,
                exp:0
            }
-       }
+       };
+       return{
+           addItem: function(type, desc, val){
+            
+            var item, id;
+
+            if(data.items[type].length === 0) id = 1;
+            else{
+                id = data.items[type][data.items[type].length - 1].id + 1;
+            }
+
+            if(type === "inc"){
+                item = new Income(id, desc, val);
+            }else{
+                item = new Expense(id, desc, val);
+            }
+
+            data.items[type].push(item);
+           },
+           seeData: function(){
+           return data;
+        }
+        };
+      
 })();
 
 //програмын холбогч контроллер
@@ -57,8 +80,10 @@ var appController = (function(uiController, financeController){
 
     var ctrlAddItem = function(){
         //1. Оруулах өгөгдлийг дэлгэцнээс авна
-        console.log(uiController.getInput());
+        var input =uiController.getInput();
+        console.log(input);
        //2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+        financeController.addItem(input.type, input.description, input.value);
        //3. Олж авсан өгөгдлүүдээ веб дээрээ тохирох хэсэгт нь гаргана
        //4.Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана
 
